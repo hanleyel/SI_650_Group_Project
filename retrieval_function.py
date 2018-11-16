15958f7e8c531291b67ec994a71633994b9491da
Akio Kakishima <akiokakishima@gmail.com>
2018-11-16 16:32:28 -0500

    Fix doc_count variable

--- retrieval_function.py
+++ retrieval_function.py
@@ -31,13 +31,15 @@ class BM25():
         with open('dataset.csv') as infile:
             csv_reader = csv.reader(infile, delimiter=',')
             for row in csv_reader:
+                # print(row)
                 num_docs += 1
                 doc = row[2]
                 doc_lst = doc.split()
-                total_dl += len(doc_lst)
-                doc_term_count = doc_lst.count(term)
-                doc_count += doc_term_count
-                doc_size = len(doc_lst)
+                total_dl += len(doc_lst) # Total length of doc
+                doc_term_count = doc_lst.count(term) # count of term in doc
+                if doc_term_count > 0:
+                    doc_count += 1 # total count of term in corpus
+                doc_size = len(doc_lst) # Total length of doc
 
         infile.close()
 
@@ -53,7 +55,8 @@ class BM25():
                 tf = ((k1+1)*doc_term_count)/(k1*(1-b+b*(doc_size/avg_dl))+doc_term_count)
                 # print(tf)
 
-                idf = math.log((num_docs-doc_count+0.5)/(doc_count+0.5)) # this line gives a math domain error when the query term is common
+                print("{}\t{}\t{}".format(num_docs, doc_count, ((num_docs-doc_count+0.5)/(doc_count+0.5))))
+                idf = math.log(((num_docs-doc_count)+0.5)/(doc_count+0.5)) # this line gives a math domain error when the query term is common
                 # print(idf)
 
                 qtf = ((k3+1)*query_term_weight)/(k3+query_term_weight)
