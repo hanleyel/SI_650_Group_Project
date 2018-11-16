ad13b8f050485c1c72e39dd796f53e595051c1bc
Elizabeth Hanley <hanley@0587348576.wireless.umich.net>
2018-11-16 15:35:28 -0500

    ...

--- retrieval_function.py
+++ retrieval_function.py
@@ -13,40 +13,33 @@ query_term_weight = 'query term count (or weight in case of feedback)'
 '''
 
 class BM25():
-    def __init__(self, k1=1.2, b=0.75, k3=500):
+    def __init__(self, k1=1.2, b=0.75, k3=500, term=input('Please enter your query term: ')):
         self.k1 = k1
         self.b = b
         self.k3 = k3
-        self.term = input('Please enter your query term: ')
-        self.num_docs = 0
-        self.total_dl = 0
-        self.doc_count = 0
-        self.query_term_weight = 1
-        with open('dataset.csv') as infile:
-            csv_reader = csv.reader(infile, delimiter=',')
-            for row in csv_reader:
-                self.num_docs += 1
-
-                doc = row[2]
-                doc_lst = doc.split()
-                self.total_dl += len(doc_lst)
-
-                self.doc_term_count = doc_lst.count(self.term)
-                self.doc_count += self.doc_term_count
-                self.doc_size = len(doc_lst)
-
-        self.avg_dl = self.total_dl / self.num_docs
+        self.term = term
 
     def scorer(self):
         k1 = self.k1
         b = self.b
         k3 = self.k3
-        doc_term_count = self.doc_term_count
-        doc_size = self.doc_size
-        avg_dl = self.avg_dl
-        num_docs = self.num_docs
-        doc_count = self.doc_count
-        query_term_weight = self.query_term_weight
+        term = self.term
+        num_docs = 0
+        total_dl = 0
+        doc_count = 0
+        query_term_weight = 1
+        with open('dataset.csv') as infile:
+            csv_reader = csv.reader(infile, delimiter=',')
+            for row in csv_reader:
+                num_docs += 1
+                doc = row[2]
+                doc_lst = doc.split()
+                total_dl += len(doc_lst)
+                doc_term_count = doc_lst.count(term)
+                doc_count += doc_term_count
+                doc_size = len(doc_lst)
+
+        avg_dl = total_dl / num_docs
 
         tf = ((k1+1)*doc_term_count)/(k1*(1-b+b*(doc_size/avg_dl))+doc_term_count)
         print(tf)
