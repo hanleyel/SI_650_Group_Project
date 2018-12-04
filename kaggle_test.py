cd5793db71c3c64ba5f3c9aa2290640b9c778efb
Akio Kakishima <akiokakishima@gmail.com>
2018-12-04 10:51:13 -0500

    Add paralellizing

--- kaggle_test.py
+++ kaggle_test.py
@@ -7,6 +7,7 @@ import os
 import csv
 import json
 import sys
+import multiprocessing
 
 akio_path = "C:/Users/akiok/Google Drive/MSI/SI650/project/SI_650_Group_Project"
 liz_path = "/Users/hanley/Desktop/SI_650_Group_Project/SI_650_Group_Project"
@@ -78,11 +79,103 @@ def metadata_extracter(path, pages_to_loop=100):
                 json_dataset[i]['keywords']
             ])
 
+def metadata_extracter_p(path, page_start, page_end, temp_csv, temp_json, csv_output):
+
+    '''
+    1) grab username/dataset info
+    2) use the username/dataset info to retrieve metadata
+    3) parse metadata for 'description', 'title', 'subtitle', 'description'
+    '''
+    print('saving data in {}'.format(path))
+
+    json_dataset = {}
+    counter = 0
+
+    ### fetch username/dataset
+    for page_num in range(page_start,page_end):
+        csv_command = 'kaggle datasets list -p{} --csv > {}.csv'.format((page_num+1), path, temp_csv)
+        os.system(csv_command)
+
+        ### parse 'ref' from csv
+        # ref should look like kmader/skin-cancer-mnist-ham10000
+        filepath = '{}/{}.csv'.format(path, temp_csv)
+        ref_list = []
+        with open(filepath, 'r', newline='') as f:
+            ref_csv = csv.DictReader(f)
+            for row in ref_csv:
+                # print(row)
+                ref_list.append(row['ref'])
+        print("\nreflist: {}\n".format(ref_list))
+        ### download the metadata using key_info into the path of your choice
+        for each_ref in ref_list:
+            print(each_ref)
+                json_command = 'kaggle datasets metadata {} -p "{}"'.format(each_ref, path)
+            os.system(json_command)
+
+            ### read through json file downloaded to grab the following:
+            # title
+            # subtitle
+            # description
+            # keywords
+
+            json_path = '{}/{}.json'.format(path, temp_json)
+            fileToUse = open(json_path, encoding = 'utf-8')
+            dataset_json = json.load(fileToUse)
+
+            ### parse json
+            json_dataset[counter] = {
+                'title': str(dataset_json['title']).encode('utf-8'),
+                'subtitle': str(dataset_json['subtitle']).encode('utf-8'),
+                'description': str(dataset_json['description']).encode('utf-8'),
+                'keywords': str(dataset_json['keywords']).encode('utf-8')
+            }
+            counter+=1
+            print(counter)
+
+    ## save dictionary as csv?
+    with open('{}/app/{}.csv'.format(path, csv_output), 'w', newline='') as f:
+        writer = csv.writer(f)
+        # writer.writerow(['title', 'subtitle', 'description', 'keywords'])
+        print(json_dataset)
+        for i in json_dataset:
+            writer.writerow([
+                json_dataset[i]['title'],
+                json_dataset[i]['subtitle'],
+                json_dataset[i]['description'],
+                json_dataset[i]['keywords']
+            ])
+
+
 if __name__ == '__main__':
-    try:
-        path = str(sys.argv[1])
-        pages = str(sys.argv[2])
-        # print("path: {}\npages: {}".format(path, pages))
-        metadata_extracter(path=path, pages_to_loop=int(pages))
-    except:
-        print("try again")
+    paralell = True
+    if paralell:
+        jobs = []
+        process = multiprocessing.Process(target=metadata_extracter_p, args=(akio_path, 1, 100, "temp", "temp", "dataset1"))
+        jobs.append(process)
+        process = multiprocessing.Process(target=metadata_extracter_p, args=(akio_path, 100, 200, "temp2", "temp2", "dataset2"))
+        jobs.append(process)
+        process = multiprocessing.Process(target=metadata_extracter_p, args=(akio_path, 200, 300, "temp3", "temp3", "dataset3"))
+        jobs.append(process)
+        process = multiprocessing.Process(target=metadata_extracter_p, args=(akio_path, 300, 400, "temp4", "temp4", "dataset4"))
+        jobs.append(process)
+        process = multiprocessing.Process(target=metadata_extracter_p, args=(akio_path, 400, 500, "temp5", "temp5", "dataset5"))
+        jobs.append(process)
+        process = multiprocessing.Process(target=metadata_extracter_p, args=(akio_path, 500, 600, "temp6", "temp6", "dataset6"))
+        jobs.append(process)
+
+        for j in jobs:
+            j.start()
+    	# Ensure all of the processes have finished
+        for j in jobs:
+            j.join()
+        print("List processing complete.")
+
+        
+    else:
+        try:
+            path = str(sys.argv[1])
+            pages = str(sys.argv[2])
+            # print("path: {}\npages: {}".format(path, pages))
+            metadata_extracter(path=path, pages_to_loop=int(pages))
+        except:
+            print("try again")
